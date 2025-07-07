b87c8b53839f0eefbc037314aca1c89bcaa140b4
htoopyaelinn56 <hplinn0@gmail.com>
2025-07-07 20:42:12 +0630

    Refactor AppBar styles and update back button SVG dimensions for improved layout

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -29,7 +29,7 @@ const AppBar: React.FC<AppBarProps> = ({title, onBackClick}) => {
             <div className="custom-appbar-left">
                 {onBackClick && (
                     <button className="custom-appbar-back" onClick={onBackClick} aria-label="Back">
-                        <svg width="48" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M15.5 19L8.5 12L15.5 5" stroke={scrolled ? "#000000" : "#ffffff"} strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
                         </svg>
                     </button>
